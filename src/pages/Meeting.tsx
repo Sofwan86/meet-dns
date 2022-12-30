027bf30dd797fbfbb98a3ddff49c85d223a9a93a
Sofwan86 <45458797+Sofwan86@users.noreply.github.com>
2022-12-30 23:01:28 +0700

    Update Meeting.tsx

--- src/pages/Meeting.tsx
+++ src/pages/Meeting.tsx
@@ -99,7 +99,7 @@ export default function Meeting() {
       render: (meetingId: string) => {
         return (
           <EuiCopy
-            textToCopy={`${process.env.REACT_APP_HOST}/join/${meetingId}`}
+            textToCopy={`https://meet-dns.vercel.app/join/${meetingId}`}
           >
             {(copy: any) => (
               <EuiButtonIcon
